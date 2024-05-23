b62590c735253547bd6ce54b9880806fa2f3ecc5
Jean Grutzmann <jeanfg86@gmail.com>
2024-05-23 01:42:02 +0000

    create entity category

--- jest.config.ts
+++ jest.config.ts
@@ -123,7 +123,7 @@ const config: Config = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  // rootDir: undefined,
+   rootDir: './src',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
---
+++ src/domain/category.entity.ts
@@ -0,0 +1,59 @@
+export type CategoryConstructorProps = {
+  category_id: string;
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+  created_at?: Date;
+};
+
+export type CategoryCreateCommand = {
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+};
+
+export class Category {
+  category_id: string;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
+
+  constructor(props: CategoryConstructorProps) {
+    this.category_id = props.category_id;
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.is_active = props.is_active ?? true;
+    this.created_at = props.created_at ?? new Date();
+  }
+
+  static create(props: CategoryConstructorProps): Category {
+    return new Category(props);
+  }
+
+  changeName(name: string): void {
+    this.name = name;
+  }
+
+  changeDescription(description: string): void {
+    this.description = description;
+  }
+
+  activate() {
+    this.is_active = true;
+  }
+
+  deactivate() {
+    this.is_active = false;
+  }
+
+  toJSON() {
+    return {
+      category_id: this.category_id,
+      name: this.name,
+      description: this.description,
+      is_active: this.is_active,
+      created_at: this.created_at,
+    };
+  }
+}
