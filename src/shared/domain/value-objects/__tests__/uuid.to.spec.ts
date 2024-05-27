b80b2787230748731585676e9d37f2b4e0ea0d51
Jean Grutzmann <jeanfg86@gmail.com>
2024-05-27 21:14:36 +0000

    should accept a valid uuid

--- src/shared/domain/value-objects/__tests__/uuid.to.spec.ts
+++ src/shared/domain/value-objects/__tests__/uuid.to.spec.ts
@@ -11,4 +11,9 @@ describe("Uuid Unit Tests", () => {
     const uuid = new Uuid();
     expect(uuid.id).toBeDefined();
   });
+
+  it("should accept a valid uuid", () => {
+    const uuid = new Uuid("92e21ae4-0247-4a93-b36a-e7fe2ce013e4");
+    expect(uuid.id).toBe("92e21ae4-0247-4a93-b36a-e7fe2ce013e4");
+  });
 });
