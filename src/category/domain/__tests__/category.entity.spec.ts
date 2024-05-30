c1612982d2e80e6ffd2a93b5a906c365ce6ba703
Jean Grutzmann <jeanfg86@gmail.com>
2024-05-30 21:30:51 +0000

    should a invalid category using description property

--- src/category/domain/__tests__/category.entity.spec.ts
+++ src/category/domain/__tests__/category.entity.spec.ts
@@ -133,12 +133,32 @@ describe("Category Unit Tests", () => {
 
   describe("Category Validator", () => {
     it("should an invalid category with name property", () => {
+      const arrange = [];
+
       expect(() => Category.create({ name: null })).containsErrorMessages({
         name: [
           "name should not be empty",
           "name must be a string",
           "name must be shorter than or equal to 255 characters",
         ],
+      });
+
+      expect(() => Category.create({ name: "" })).containsErrorMessages({
+        name: ["name should not be empty"],
+      });
+
+      expect(() => Category.create({ name: 5 as any })).containsErrorMessages({
+        name: ["name must be a string", "name must be shorter than or equal to 255 characters"],
+      });
+
+      expect(() => Category.create({ name: "t".repeat(256) })).containsErrorMessages({
+        name: ["name must be shorter than or equal to 255 characters"],
+      });
+    });
+
+    it("should a invalid category using description property", () => {
+      expect(() => Category.create({ description: 5 } as any)).containsErrorMessages({
+        description: ["description must be a string"],
       });
     });
   });
