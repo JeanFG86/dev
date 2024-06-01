32900ae2f1adc0f91c811d796a08de55b0979f8e
Jean Grutzmann <jeanfg86@gmail.com>
2024-06-01 23:58:55 +0000

    should insert a new entity

---
+++ src/shared/db/in-memory/in-memory.repository.spec.ts
@@ -0,0 +1,57 @@
+import { Entity } from "../../domain/entity";
+import { ValueObject } from "../../domain/value-object";
+import { Uuid } from "../../domain/value-objects/uuid.vo";
+import { InMemoryRepository } from "./in-memory.repository";
+
+type StubEntityConstructor = {
+  entity_id?: Uuid;
+  name: string;
+  price: number;
+};
+
+class StubEntity extends Entity {
+  entity_id: Uuid;
+  name: string;
+  price: number;
+
+  constructor(props: StubEntityConstructor) {
+    super();
+    this.entity_id = props.entity_id || new Uuid();
+    this.name = props.name;
+    this.price = props.price;
+  }
+
+  toJSON() {
+    return {
+      entity_id: this.entity_id,
+      name: this.name,
+      price: this.price,
+    };
+  }
+}
+
+class StubInMemoryRepository extends InMemoryRepository<StubEntity, Uuid> {
+  getEntity(): new (...args: any[]) => StubEntity {
+    return StubEntity;
+  }
+}
+
+describe("InMemoryRepository Unit Tests", () => {
+  let repo: StubInMemoryRepository;
+
+  beforeEach(() => {
+    repo = new StubInMemoryRepository();
+  });
+  it("should insert a new entity", async () => {
+    const entity = new StubEntity({
+      entity_id: new Uuid(),
+      name: "Test",
+      price: 100,
+    });
+
+    await repo.insert(entity);
+
+    expect(repo.items.length).toBe(1);
+    expect(repo.items[0]).toBe(entity);
+  });
+});
