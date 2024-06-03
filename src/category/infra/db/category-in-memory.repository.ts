19222b6b58dd8bf4d9ec73ee98ce674b9977dbe3
Jean Grutzmann <jeanfg86@gmail.com>
2024-06-03 22:19:42 +0000

    apply filters

--- src/category/infra/db/category-in-memory.repository.ts
+++ src/category/infra/db/category-in-memory.repository.ts
@@ -1,9 +1,32 @@
+import { SortDirection } from "../../../shared/domain/repository/search-params";
 import { Uuid } from "../../../shared/domain/value-objects/uuid.vo";
-import { InMemoryRepository } from "../../../shared/infra/db/in-memory/in-memory.repository";
+import {
+  InMemoryRepository,
+  InMemorySearchableRepository,
+} from "../../../shared/infra/db/in-memory/in-memory.repository";
 import { Category } from "../../domain/category.entity";
+import { CategoryFilter, ICategoryRepository } from "../../domain/category.repository";
 
-export class CategoryInMemoryRepository extends InMemoryRepository<Category, Uuid> {
+export class CategoryInMemoryRepository
+  extends InMemorySearchableRepository<Category, Uuid>
+  implements ICategoryRepository
+{
+  sortableFields: string[] = ["name", "created_at"];
+
+  protected async applyFilter(items: Category[], filter: CategoryFilter): Promise<Category[]> {
+    if (!filter) {
+      return items;
+    }
+
+    return items.filter((i) => {
+      return i.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
   getEntity(): new (...args: any[]) => Category {
     return Category;
+  }
+
+  protected applySort(items: Category[], sort: string | null, sort_dir: SortDirection | null) {
+    return sort ? super.applySort(items, sort, sort_dir) : super.applySort(items, "created_at", "desc");
   }
 }
