2a7b17730ca79ea9fc2a28530aec9415c47bf271
Jean Grutzmann <jeanfg86@gmail.com>
2024-06-05 23:41:43 +0000

    create CategorySequelizeRepository

--- src/category/infra/db/sequelize/category.model.ts
+++ src/category/infra/db/sequelize/category.model.ts
@@ -1,7 +1,15 @@
 import { Column, DataType, Model, PrimaryKey, Table } from "sequelize-typescript";
 
+export type CategoryModelProps = {
+  category_id: string;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
+};
+
 @Table({ tableName: "categories", timestamps: false })
-export class CategoryModel extends Model {
+export class CategoryModel extends Model<CategoryModelProps> {
   @PrimaryKey
   @Column({ type: DataType.UUID })
   declare category_id: string;
