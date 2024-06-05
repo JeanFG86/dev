3c1e2c9bcf7e2a05fa333d7528e530df347ecf70
Jean Grutzmann <jeanfg86@gmail.com>
2024-06-05 23:02:13 +0000

    create category model

--- src/category/infra/db/in-memory/category-in-memory.repository.ts
+++ src/category/infra/db/in-memory/category-in-memory.repository.ts
@@ -1,11 +1,11 @@
-import { SortDirection } from "../../../shared/domain/repository/search-params";
-import { Uuid } from "../../../shared/domain/value-objects/uuid.vo";
+import { SortDirection } from "../../../../shared/domain/repository/search-params";
+import { Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
 import {
   InMemoryRepository,
   InMemorySearchableRepository,
-} from "../../../shared/infra/db/in-memory/in-memory.repository";
-import { Category } from "../../domain/category.entity";
-import { CategoryFilter, ICategoryRepository } from "../../domain/category.repository";
+} from "../../../../shared/infra/db/in-memory/in-memory.repository";
+import { Category } from "../../../domain/category.entity";
+import { CategoryFilter, ICategoryRepository } from "../../../domain/category.repository";
 
 export class CategoryInMemoryRepository
   extends InMemorySearchableRepository<Category, Uuid>
