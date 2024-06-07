7a9867d9fa4db26e1271acf66eb79a7238e791d4
Jean Grutzmann <jeanfg86@gmail.com>
2024-06-07 00:11:17 +0000

    update, delete, search

--- src/category/infra/db/sequelize/category-sequelize.repository.ts
+++ src/category/infra/db/sequelize/category-sequelize.repository.ts
@@ -1,3 +1,5 @@
+import { Op } from "sequelize";
+import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
 import { Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
 import { Category } from "../../../domain/category.entity";
 import { CategorySearchParams, CategorySearchResult, ICategoryRepository } from "../../../domain/category.repository";
@@ -28,14 +30,35 @@ export class CategorySequelizeRepository implements ICategoryRepository {
       }))
     );
   }
-  updade(entity: Category): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async updade(entity: Category): Promise<void> {
+    const id = entity.category_id.id;
+    const model = await this._get(id);
+    if (!model) {
+      throw new NotFoundError(id, this.getEntity());
+    }
+    await this.categoryModel.update(
+      {
+        category_id: entity.category_id.id,
+        name: entity.name,
+        description: entity.description,
+        is_active: entity.is_active,
+        created_at: entity.created_at,
+      },
+      { where: { category_id: id } }
+    );
   }
-  delete(entity_id: Uuid): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async delete(category_id: Uuid): Promise<void> {
+    const id = category_id.id;
+    const model = await this._get(id);
+    if (!model) {
+      throw new NotFoundError(id, this.getEntity());
+    }
+    await this.categoryModel.destroy({ where: { category_id: id } });
   }
   async findById(entity_id: Uuid): Promise<Category | null> {
-    const model = await this.categoryModel.findByPk(entity_id.id);
+    const model = await this._get(entity_id.id);
     return new Category({
       category_id: new Uuid(model.category_id),
       name: model.name,
@@ -44,6 +67,11 @@ export class CategorySequelizeRepository implements ICategoryRepository {
       created_at: model.created_at,
     });
   }
+
+  private async _get(id: string) {
+    return await this.categoryModel.findByPk(id);
+  }
+
   async findAll(): Promise<Category[]> {
     const models = await this.categoryModel.findAll();
     return models.map((model) => {
@@ -56,8 +84,35 @@ export class CategorySequelizeRepository implements ICategoryRepository {
       });
     });
   }
-  search(props: CategorySearchParams): Promise<CategorySearchResult> {
-    throw new Error("Method not implemented.");
+  async search(props: CategorySearchParams): Promise<CategorySearchResult> {
+    const offset = (props.page - 1) * props.per_page;
+    const limit = props.per_page;
+    const { rows: models, count } = await this.categoryModel.findAndCountAll({
+      ...(props.filter && {
+        where: {
+          name: { [Op.like]: `%${props.filter}%` },
+        },
+      }),
+      ...(props.sort && this.sortableFields.includes(props.sort)
+        ? { order: [[props.sort, props.sort_dir]] }
+        : { order: [["created_at", "desc"]] }),
+      offset,
+      limit,
+    });
+    return new CategorySearchResult({
+      items: models.map((model) => {
+        return new Category({
+          category_id: new Uuid(model.category_id),
+          name: model.name,
+          description: model.description,
+          is_active: model.is_active,
+          created_at: model.created_at,
+        });
+      }),
+      current_page: props.page,
+      per_page: props.per_page,
+      total: count,
+    });
   }
   getEntity(): new (...args: any[]) => Category {
     return Category;
